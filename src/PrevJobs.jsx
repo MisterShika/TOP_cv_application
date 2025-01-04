0b2f990b842544f4b72b495d3d4edd0b5cf21461
Kyle W <125979751+MisterShika@users.noreply.github.com>
2025-01-04 17:54:05 +0900

    Job list now able to be displayed.

--- src/PrevJobs.jsx
+++ src/PrevJobs.jsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
+
+function SinglePrevJob ({jobInfo, updateJobList}) {
+    return(
+        <li>
+            <span>{jobInfo.name}</span>
+        </li>
+    )
+}
+
+function PrevJobList ({jobs, updateJobList}) {
+    return (
+        <ul>
+            {jobs.map( singleJob => (
+                <SinglePrevJob key={singleJob.id} jobInfo={singleJob} updateJobList={updateJobList} />
+            ))}
+        </ul>
+    );
+}
+
 function PrevJobs () {
+    const [jobs, setJobs] = useState([]);
+
+    const updateJobList = () => {
+        // const newJob = {...jobs, }
+    };
+
+    const addJob = () => {
+        const newJob = {id: crypto.randomUUID(), name: `Job ${jobs.length + 1}` };
+        setJobs([...jobs, newJob]);
+    };
+
     return (
         <>
-            Hello World!
+            <PrevJobList jobs={jobs} updateJobList={updateJobList} />
+            <button onClick={addJob} />
         </>
     );
 }
